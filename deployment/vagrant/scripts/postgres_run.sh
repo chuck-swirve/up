b02a550e5948cc526749782ac36ec014a30bf413
chuck-swirve <chuck-swirve@users.noreply.github.com>
2016-07-30 22:26:41 -0400

    UTC is good. UTF-8 is good. It is all good

--- deployment/vagrant/scripts/postgres_run.sh
+++ deployment/vagrant/scripts/postgres_run.sh
@@ -13,4 +13,10 @@ CREATE USER $LOCAL_DB_USER WITH PASSWORD '$LOCAL_DB_PASSWORD';
 ALTER USER $LOCAL_DB_USER WITH CREATEDB;
 -- Give the user permission to create schemas on db
 GRANT CREATE ON DATABASE $LOCAL_DB_NAME TO $LOCAL_DB_USER;
+-- UTC or bust
+ALTER ROLE $LOCAL_DB_USER SET timezone TO 'UTC';
+-- UTF-8 or bust
+ALTER ROLE $LOCAL_DB_USER SET client_encoding TO 'utf8';
+-- Copypasta. Isn't this the default?
+ALTER ROLE $LOCAL_DB_USER SET default_transaction_isolation TO 'read committed';
 EOF
