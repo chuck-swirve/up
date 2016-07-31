ba6356931740496fe0c1d8f5ebd7b3b58596b7ae
chuck-swirve <chuck-swirve@users.noreply.github.com>
2016-07-31 00:49:55 -0400

    Various minor changes to models
    - Added __repr__ methods for all models
    
    - Added related_name for FK relationships
    
    - Defined FK from Post to Thread
    
    - Generated migration for the above
    
    Signing off for the night now. 4 hours logged today at comfortable
    effort without any real urgency.

--- up/apps/forums/models.py
+++ up/apps/forums/models.py
@@ -19,14 +19,27 @@ class Forum(BaseModel):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=50, unique=True)
 
+    def __repr__(self):
+        return '<Forum: {}>'.format(self.name)
+
 
 class Thread(BaseModel):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
-    forum = models.ForeignKey(Forum)
+    forum = models.ForeignKey(Forum, related_name='threads')
     is_sticky = models.BooleanField(default=False)
+
+    def __repr__(self):
+        return '<Thread: {}>'.format(self.title)
 
 
 class Post(BaseModel):
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
     content = models.TextField()
+    thread = models.ForeignKey(Thread, related_name='posts')
+
+    class Meta:
+        ordering = ['created']
+
+    def __repr__(self):
+        return '<Post: {}>'.format(self.pk)
