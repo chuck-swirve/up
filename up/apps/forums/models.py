3618ec8bdb0300b17c7da86591fd42e7ec34305d
chuck-swirve <chuck-swirve@users.noreply.github.com>
2016-07-30 23:36:49 -0400

    Django models for forums

--- up/apps/forums/models.py
+++ up/apps/forums/models.py
@@ -1,5 +1,32 @@
-from __future__ import unicode_literals
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+        ordering = ['-updated']
+
+
+class Forum(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+
+class Thread(BaseModel):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    forum = models.ForeignKey(Forum)
+    is_sticky = models.BooleanField(default=False)
+
+
+class Post(BaseModel):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    content = models.TextField()
