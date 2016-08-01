7f488d1bd9c2815a8375d71d9f28962fba8d7b1a
chuck-swirve <chuck-swirve@users.noreply.github.com>
2016-07-31 20:17:09 -0400

    Name views in forums.urls

--- up/apps/forums/urls.py
+++ up/apps/forums/urls.py
@@ -7,8 +7,13 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.get_forum_list),
-    url(r'^boards/(?P<forum_slug>[-\w]+)/$', views.get_forum_detail),
+    url(r'^$',
+        views.get_forum_list,
+        name='get-forum-list'),
+    url(r'^boards/(?P<forum_slug>[-\w]+)/$',
+        views.get_forum_detail,
+        name='get-forum-detail'),
     url(r'^boards/(?P<forum_slug>[-\w]+)/(?P<thread_id>[0-9])/$',
-        views.get_thread_detail),
+        views.get_thread_detail,
+        name='get-thread-detail'),
 ]
