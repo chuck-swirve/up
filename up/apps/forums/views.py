9fe0da01b32e469af1e1ec9196bdbfb1ebcf0bc5
chuck-swirve <chuck-swirve@users.noreply.github.com>
2016-07-31 20:17:27 -0400

    Oops. Import get_object_or_404

--- up/apps/forums/views.py
+++ up/apps/forums/views.py
@@ -2,7 +2,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Forum, Post, Thread
 
