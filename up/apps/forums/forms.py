35fe5860086bc2a255f859a3aa2b21fe8787baef
chuck-swirve <chuck-swirve@users.noreply.github.com>
2016-08-06 16:56:50 -0400

    Make forms more up-like

--- up/apps/forums/forms.py
+++ up/apps/forums/forms.py
@@ -10,9 +10,21 @@ class ThreadForm(forms.ModelForm):
     class Meta:
         model = models.Thread
         fields = ['title']
+        widgets = {
+            'title': forms.TextInput(attrs={'size': '20'}),
+        }
+        labels = {
+            'title': 'Topic Name'
+        }
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = models.Post
         fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': '10', 'cols': '50'}),
+        }
+        labels = {
+            'content': 'Your Message'
+        }
