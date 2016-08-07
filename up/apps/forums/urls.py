ba8e35aa1337ddfcaa1771ae9fefb07f04454017
chuck-swirve <chuck-swirve@users.noreply.github.com>
2016-08-06 23:24:52 -0400

    Display login form with post/thread forms

--- up/apps/forums/urls.py
+++ up/apps/forums/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^boards/(?P<forum_slug>[-\w]+)/$',
         views.ForumDetailView.as_view(),
         name='get-forum-detail'),
-    url(r'^boards/(?P<forum_slug>[-\w]+)/(?P<thread_id>[0-9])/$',
+    url(r'^boards/(?P<forum_slug>[-\w]+)/(?P<thread_id>[0-9]+)/$',
         views.ThreadDetailView.as_view(),
         name='get-thread-detail'),
 ]
--- up/apps/forums/views.py
+++ up/apps/forums/views.py
@@ -4,6 +4,7 @@ from __future__ import (absolute_import, division, print_function,
 from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
 from django.contrib.auth import login
+from django.contrib.auth import logout
 from django.db import transaction
 from django.db import Error as DatabaseError
 from django.db.models import F
@@ -20,6 +21,43 @@ from .models import Thread
 
 
 User = get_user_model()
+
+
+def _get_login_form(request):
+    if request.user.is_authenticated():
+        return LoginForm(initial={
+            'username': request.user.username,
+        })
+    else:
+        return LoginForm()
+
+
+def _get_user_lazy_login(request, context):
+    login_form = LoginForm(request.POST)
+    context['login_form'] = login_form
+    is_valid = login_form.is_valid()
+    is_authenticated = request.user.is_authenticated()
+    if is_authenticated and not is_valid:
+        # no new credentials provided
+        return request.user
+    elif is_authenticated and is_valid:
+        # new credentials provided
+        logout(request)
+
+    if is_valid:
+        username = login_form.cleaned_data['username']
+        password = login_form.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+        if user is None:
+            msg = 'That user/password combination is not recognized'
+            login_form.add_error(None, msg)
+        elif user is not None and not user.is_active:
+            msg = 'Your account is disabled.'
+            login_form.add_error(None, msg)
+        elif user is not None and user.is_active:
+            login(request, user)
+            return user
+    return None
 
 
 class ForumListView(View):
@@ -44,38 +82,38 @@ class ForumDetailView(View):
             'threads': threads,
             'thread_form': ThreadForm(),
             'post_form': PostForm(),
-            'login_form': LoginForm(),
+            'login_form': _get_login_form(request),
         }
         return render(request, self.template_name, context=context)
 
     def post(self, request, forum_slug, *args, **kwargs):
-        user = User.objects.first()
         forum = get_object_or_404(Forum, slug=forum_slug)
         # we only need `threads` if the form isn't valid.
         # this is inefficient in the common case
         threads = forum.threads.filter(is_deleted=False)[:100]
         thread_form = ThreadForm(request.POST)
         post_form = PostForm(request.POST)
-        login_form = LoginForm()
         context = {
             'forum': forum,
             'threads': threads,
             'thread_form': thread_form,
             'post_form': post_form,
-            'login_form': login_form,
         }
+        user = _get_user_lazy_login(request, context)
+        if user is None:
+            return render(request, self.template_name, context=context)
         if thread_form.is_valid() and post_form.is_valid():
             try:
                 with transaction.atomic():
                     new_thread = thread_form.save(commit=False)
-                    new_thread.author = user
+                    new_thread.author = request.user
                     new_thread.post_count = 1
                     new_thread.forum = forum
                     forum.post_count = F('post_count') + 1
                     new_thread.save()
 
                     new_post = post_form.save(commit=False)
-                    new_post.author = user
+                    new_post.author = request.user
                     new_post.thread = new_thread
 
                     new_post.save()
@@ -104,23 +142,26 @@ class ThreadDetailView(View):
             'thread': thread,
             'posts': posts,
             'post_form': PostForm(),
-            'login_form': LoginForm(),
+            'login_form': _get_login_form(request),
         }
         return render(request, self.template_name, context=context)
 
     def post(self, request, forum_slug, thread_id, *args, **kwargs):
-        user = User.objects.first()
         forum = get_object_or_404(Forum, slug=forum_slug)
         thread = get_object_or_404(forum.threads, id=thread_id)
         # we only need `posts` if the form isn't valid.
         # this is inefficient in the common case
         posts = thread.posts.all()
         post_form = PostForm(request.POST)
-        login_form = LoginForm()
         context = {
             'forum': forum,
             'thread': thread,
+            'posts': posts,
+            'post_form': post_form,
         }
+        user = _get_user_lazy_login(request, context)
+        if user is None:
+            return render(request, self.template_name, context)
         try:
             with transaction.atomic():
                 thread.refresh_from_db()
